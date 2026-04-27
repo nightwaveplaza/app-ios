38ea8a9e572d30bc8bdfe8486b7b9d9c9f9579f5
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2026-04-28 05:55:27 +0600

    Remove unused code

--- NightwavePlaza/Network/Models/WebMessage.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WebMessage.swift
-//  NightwavePlaza
-//
-//  Created by Aleksey Garbarev on 15.08.2020.
-//  Copyright © 2020 Aleksey Garbarev. All rights reserved.
-//
-
-import Foundation
