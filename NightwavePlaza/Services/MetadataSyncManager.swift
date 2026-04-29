131de32fab0e39769f215b555ac5dd3c0d31a5aa
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2026-04-29 07:45:01 +0600

    Calculate actual position on status update

--- NightwavePlaza/Services/MetadataSyncManager.swift
+++ NightwavePlaza/Services/MetadataSyncManager.swift
@@ -5,6 +5,7 @@
 //  Created by Alexander on 27.04.2026.
 //  Copyright © 2026 Alexander Morozov. All rights reserved.
 //
+
 import Foundation
 
 actor MetadataSyncManager {
@@ -27,9 +28,21 @@ actor MetadataSyncManager {
             
             if status.song.id.isEmpty { return }
             
+            let now = Date().timeIntervalSince1970
+            let serverTime = Double(status.updatedAt)
+            let networkDelay = now - serverTime
+            let safeDelay = max(0, networkDelay)
+            
+            let duration = Double(status.song.length)
+            
+            var actualPosition = Double(status.position) + safeDelay
+            actualPosition = min(actualPosition, duration)
+            
             await playerService?.updateMetadata(
                 title: status.song.title,
                 artist: status.song.artist,
+                duration: duration,
+                position: actualPosition,
                 coverUrl: URL(string: status.song.artworkSrc)
             )
         } catch {
